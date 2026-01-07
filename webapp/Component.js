aca54e91e7cb28b73dcc39ff0ebf72f949d1516c
hyejin jeon <nueiin630@gmail.com>
2026-01-07 12:22:11 +0000

    initial product list app

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.nueiin.fiori.product.productlist.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
